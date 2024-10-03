bddeda7e6ed5ca599306a53ded21984b356269b5
Alex Christie <140353863+alexchristie12@users.noreply.github.com>
2024-10-03 21:27:01 +1000

    Implement float loading and unloading functions

--- src/drivers/config_loader/config_loader.c
+++ src/drivers/config_loader/config_loader.c
@@ -1,15 +1,43 @@
 #include "drivers/config_loader/config_loader.h"
 #include <stdint.h>
 
-
 static inline void load_registers_from_16_bit(uint16_t number, uint8_t* addr) {
     addr[0] = number & 0xFF;
-    addr[1] = (number >> 8) & 0xFF; 
+    addr[1] = (number >> 8) & 0xFF;
 }
 
-static inline void load_registers_from_32_bit(uint16_t number, uint8_t* addr) {
+static inline void load_registers_from_32_bit(uint32_t number, uint8_t* addr) {
     addr[0] = number & 0xFF;
-    addr[1] = (number >> 8) & 0xFF; 
-    addr[2] = (number >> 16) & 0xFF; 
-    addr[3] = (number >> 24) & 0xFF; 
+    addr[1] = (number >> 8) & 0xFF;
+    addr[2] = (number >> 16) & 0xFF;
+    addr[3] = (number >> 24) & 0xFF;
+}
+
+static inline uint32_t load_32_bit_from_registers(uint8_t* registers) {
+    return (uint32_t)registers[0] | ((uint32_t)registers[1] << 8) | ((uint32_t)registers[2] << 16) | ((uint32_t)registers[3] << 24);
+}
+
+static inline uint16_t load_16_bit_from_registers(uint8_t* registers) {
+    return (uint16_t)registers[0] | ((uint16_t)registers[1] << 8);
+}
+
+static inline void load_registers_from_float(float number, uint8_t* addr) {
+    // This is brilliant
+    union
+    {
+        float f;
+        uint32_t u;
+    } float_union;
+
+    float_union.f = number;
+    load_registers_from_32_bit(float_union.u, addr);
+}
+
+static inline float load_float_from_registers(uint8_t* registers) {
+    union {
+        float f;
+        uint32_t u;
+    } float_union;
+    float_union.u = load_32_bit_from_registers(registers);
+    return float_union.f;
 }
--- src/drivers/config_loader/config_loader.h
+++ src/drivers/config_loader/config_loader.h
@@ -8,6 +8,12 @@ flash memory
 
 #include <stdint.h>
 
+typedef enum {
+    TEMPERATURE,
+    HUMIDITY,
+    SOIL_MOISTURE,
+} sensor_type;
+
 typedef struct {
    uint32_t hardware_id;
    uint32_t dob_unix;  
@@ -27,11 +33,6 @@ typedef struct {
     uint16_t adc_max_map;
 } adc_config;
 
-typedef enum {
-    TEMPERATURE,
-    HUMIDITY,
-    SOIL_MOISTURE,
-} sensor_type;
 
 typedef struct {
     general_config general_config;
@@ -42,14 +43,30 @@ typedef struct {
 /// @brief Load a 16 bit unsigned integer from the config registers 
 /// @param registers The registers, must be length 2
 /// @return Return the loaded 16 bit unsigned integer
-static uint16_t load_16_bit_from_registers(uint8_t *registers);
+static inline uint16_t load_16_bit_from_registers(uint8_t *registers);
 
 /// @brief Load a 32 bit unsigned integer from the config registers 
 /// @param registers The registers, must be a length 4
 /// @return Return the loaded 32 bit unsigned integer
-static uint32_t load_32_bit_from_registers(uint8_t *registers);
+static inline uint32_t load_32_bit_from_registers(uint8_t *registers);
 
 
-static inline void load_registers_from_32_bit(uint16_t number, uint8_t* addr);
+/// @brief Load 2 8-bit registers from a single unsigned 16 bit integer.
+/// @param number The number to be loaded
+/// @param addr The address of the base register
+static inline void load_registers_from_32_bit(uint32_t number, uint8_t* addr);
 
+/// @brief Load 4 8-bit registers from a single unsigned 32 bit integer.
+/// @param number The number to be loaded
+/// @param addr The address of the base register
 static inline void load_registers_from_16_bit(uint16_t number, uint8_t* addr);
+
+/// @brief Load 4 8-bit registers from a float
+/// @param number The number to be loaded
+/// @param addr The register to be loaded
+static inline void load_registers_from_float(float number, uint8_t* addr);
+
+/// @brief Load a floating point number from 4 8-bit registers
+/// @param registers The base address of the registers, length of 4
+/// @return The float stored in these registers
+static inline float load_float_from_registers(uint8_t* registers);
