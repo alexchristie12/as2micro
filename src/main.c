b07664df64ac0af1b4a85229096b3099b88a96f5
lexieg77 <lexie.giandon@my.jcu.edu.au>
2024-10-16 13:57:10 +1000

    takes data arrays and creates output data string for transmision over bluetooth

--- src/main.c
+++ src/main.c
@@ -11,9 +11,36 @@
 
 int main() {
     io_init();
+    // 5 zones
+    int offset = 0;
+    float temp[] = {23, 23.2, 23.1, 23}; // 4 readings from 4 different temp sensors
+    float humidity[] = {76, 75.4, 75.8};
+    float soil_moisture[] = {40, 40.3};
+    float flow_rate = 46.3;
+    bool water_on; // tell the pi if the water is running (1) or not (0)
+    int zone_number = 1; // alex config to do
 
+    void sensor_data (int zone_number){
+        // Rotate through for each zone:
+        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "zone:%i,floaw_rate:%.2f,water_on:%d", zone_number, flow_rate, water_on);
+        // Temperature Sensors:
+        for (int i = 0; i < sizeof(temp); i++) {
+            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "temp%i:%.2f,", i, temp[i]);
+        }
+        // Humidity Sensors:
+        for (int i = 0; i < sizeof(humidity); i++) {
+            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "humidity%i:%.2f,", i, humidity[i]);
+        }
+        // Soil Moisture Sensors:
+        for (int i = 0; i < sizeof(soil_moisture); i++) {
+            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "soil_moisture%i:%.2f", i, soil_moisture[i]);
+        }
+    }
+   
     for (;;) {
-        
+        char buffer[1024];
+        sensor_data(zone_number);
+        uart_puts(UART_ID, buffer); 
     }
     return 0;
 }
