7cd5840d6be8487fed127182c9a2b112527d742c
Brennen12 <brennen.caruana@my.jcu.edu.au>
2024-10-16 15:42:45 +1000

    moved function out of main

--- src/main.c
+++ src/main.c
@@ -9,39 +9,40 @@
 #include "drivers/WS2812/led.h"
 #include "drivers/io/io.h"
 
+// 5 zones
+int   offset          = 0;
+float temperature[]   = {23, 23.2, 23.1, 23}; // 4 readings from 4 different temp sensors
+float humidity[]      = {76, 75.4, 75.8};
+float soil_moisture[] = {40, 40.3};
+float flow_rate       = 46.3;
+bool  water_on;        // tell the pi if the water is running (1) or not (0)
+int   zone_number = 1; // alex config to do
+char buffer[1024];
+
+void sensor_data(int zone_number) {
+    // Rotate through for each zone:
+    offset += snprintf(buffer + offset, sizeof(buffer) - offset, "zone=%i,", zone_number);
+    // Temperature Sensors:
+    for (int i = 0; i < sizeof(temperature); i++) {
+        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "temperature%i=%.2f,", i, temperature[i]);
+    }
+    // Humidity Sensors:
+    for (int i = 0; i < sizeof(humidity); i++) {
+        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "humidity%i=%.2f,", i, humidity[i]);
+    }
+    // Soil Moisture Sensors:
+    for (int i = 0; i < sizeof(soil_moisture); i++) {
+        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "soil_moisture%i=%.2f,", i, soil_moisture[i]);
+    }
+    offset += snprintf(buffer + offset, sizeof(buffer) - offset, "flow_rate=%.2f,water_on=%d", flow_rate, water_on);
+}
+
 int main() {
     io_init();
-    // 5 zones
-    int offset = 0;
-    float temperature[] = {23, 23.2, 23.1, 23}; // 4 readings from 4 different temp sensors
-    float humidity[] = {76, 75.4, 75.8};
-    float soil_moisture[] = {40, 40.3};
-    float flow_rate = 46.3;
-    bool water_on; // tell the pi if the water is running (1) or not (0)
-    int zone_number = 1; // alex config to do
 
-    void sensor_data (int zone_number){
-        // Rotate through for each zone:
-        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "zone=%i,", zone_number);
-        // Temperature Sensors:
-        for (int i = 0; i < sizeof(temp); i++) {
-            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "temperature%i=%.2f,", i, temperature[i]);
-        }
-        // Humidity Sensors:
-        for (int i = 0; i < sizeof(humidity); i++) {
-            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "humidity%i=%.2f,", i, humidity[i]);
-        }
-        // Soil Moisture Sensors:
-        for (int i = 0; i < sizeof(soil_moisture); i++) {
-            offset += snprintf(buffer + offset, sizeof(buffer) - offset, "soil_moisture%i=%.2f,", i, soil_moisture[i]);
-        }
-        offset += snprintf(buffer + offset, sizeof(buffer) - offset, "flow_rate=%.2f,water_on=%d", flow_rate, water_on);
-    }
-   
     for (;;) {
-        char buffer[1024];
         sensor_data(zone_number);
-        uart_puts(UART_ID, buffer); 
+        uart_puts(UART_ID, buffer);
     }
     return 0;
 }
